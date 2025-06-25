d206ac9a945bf44eaf39043bf0879f95f6f3d63d
daviddodo <hanhduy11@hotmail.com>
2025-06-24 23:39:32 -0400

    forgot some tags in contribution type controller

--- sponsors/sonia/src/main/java/com/mti825/sonia/controllers/ContributionTypeController.java
+++ sponsors/sonia/src/main/java/com/mti825/sonia/controllers/ContributionTypeController.java
@@ -3,12 +3,16 @@ package com.mti825.sonia.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mti825.sonia.dto.ContributionTypeDto;
 import com.mti825.sonia.dto.ContributionTypeResponse;
 import com.mti825.sonia.services.ContributionTypeService;
+
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +32,8 @@ public class ContributionTypeController {
      * @return the created contribution type response
      */
     @PostMapping()
-    public ContributionTypeResponse createContributionType(@RequestBody ContributionTypeDto contributionTypeDto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContributionTypeResponse createContributionType(@Valid @RequestBody ContributionTypeDto contributionTypeDto) {
         return contributionTypeService.createContributionType(contributionTypeDto);
     }
     
