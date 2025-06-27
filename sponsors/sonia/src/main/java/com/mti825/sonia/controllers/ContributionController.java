9ae74f2bf2a3d41783b682178ee383855bd988ce
daviddodo <hanhduy11@hotmail.com>
2025-06-27 01:21:15 -0400

    get all contributions or by id, delete by id

--- sponsors/sonia/src/main/java/com/mti825/sonia/controllers/ContributionController.java
+++ sponsors/sonia/src/main/java/com/mti825/sonia/controllers/ContributionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mti825.sonia.dto.ContributionDto;
@@ -15,11 +16,12 @@ import com.mti825.sonia.services.ContributionService;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("/contributions")
@@ -33,8 +35,42 @@ public class ContributionController {
      * @return the created contribution response
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ContributionResponse createContribution(@RequestBody @Valid ContributionDto contributionDto) {
         return contributionService.createContribution(contributionDto);
+    }
+
+    /**     * Retrieves all contacts.
+     *
+     * This method returns a list of all contacts in the system.
+     * It is useful for displaying all contacts on a page or in a list.
+     * @return a list of ContactResponse objects representing all contacts
+     */
+    @GetMapping
+    public List<ContributionResponse> getAllContributions() {
+        return contributionService.getAllContributions();
+    }
+
+    /**     * Retrieves a contribution by its ID.
+     *
+     * @param id the ID of the contribution to retrieve
+     * @return the ContributionResponse DTO
+     * @throws IllegalArgumentException if no contribution is found with the given ID
+     */
+    @GetMapping(value = "/{id}")
+    public ContributionResponse getContributionById(@PathVariable Long id) {
+        return contributionService.getContributionById(id);
+    }
+    
+    /**     * Deletes a contribution by its ID.
+     *
+     * This method will throw an IllegalArgumentException if no contribution is found with the given ID.
+     * @param id the ID of the contribution to delete
+     */
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteContributionById(@PathVariable Long id) {
+        contributionService.deleteContributionById(id);
     }
 
     /**     * Retrieves all contribution types.
--- sponsors/sonia/src/main/java/com/mti825/sonia/services/ContributionService.java
+++ sponsors/sonia/src/main/java/com/mti825/sonia/services/ContributionService.java
@@ -1,5 +1,7 @@
 package com.mti825.sonia.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,43 @@ public class ContributionService {
         return new ContributionResponse(contribution);
     }
 
+    /**     * Retrieves all contributions.
+     *
+     * This method returns a list of all contributions in the system.
+     * It is useful for displaying all contributions on a page or in a list.
+     * @return a list of ContributionRespnse objects representing all contributions
+     */
+    public List<ContributionResponse> getAllContributions() {
+        List<Contribution> contributions = contributionRepository.findAll();
+
+        return mapToResponseList(contributions);
+    }
+
+    /**     * Retrieves a contribution by its ID.
+     *
+     * @param id the ID of the contribution to retrieve
+     * @return the ContributionResponse DTO
+     * @throws IllegalArgumentException if no contribution is found with the given ID
+     */
+    public ContributionResponse getContributionById(Long id) {
+        Contribution contribution = contributionRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Contribution not found with id " + id));
+
+        return new ContributionResponse(contribution);
+    }
+
+    /**     * Deletes a contribution by its ID.
+     *
+     * This method will throw an IllegalArgumentException if no contribution is found with the given ID.
+     * @param id the ID of the contribution to delete
+     */
+    public void deleteContributionById(Long id) {
+        if (!contributionRepository.existsById(id)) {
+            throw new IllegalArgumentException("Contribution not found with id: " + id);
+        }
+        contributionRepository.deleteById(id);
+    }
+
     /**
      * Maps a ContributionDto to a Contribution entity.
      *
@@ -37,5 +76,16 @@ public class ContributionService {
         Contribution contribution = new Contribution(contributionDto);
 
         return contribution;
+    }
+
+    /**     * Maps a list of Contact entities to a list of ContactResponse DTOs.
+     *
+     * @param contacts the list of Contact entities to map
+     * @return a list of ContactResponse DTOs
+     */
+    private List<ContributionResponse> mapToResponseList(List<Contribution> contributions) {
+        return contributions.stream()
+            .map(ContributionResponse::new)
+            .toList();
     }
 }
